dd3e1cc7275b35ec0cafc8c678034223d9a6b2c8
steven-yn <ssungyeon94@gmail.com>
2025-02-13 23:20:01 +0900

    chore: add dialog

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -7,6 +7,7 @@ export * from "./src/components/ui/button";
 export * from "./src/components/ui/card";
 export * from "./src/components/ui/calendar";
 export * from "./src/components/ui/checkbox";
+export * from "./src/components/ui/dialog";
 export * from "./src/components/ui/input";
 export * from "./src/components/ui/label";
 export * from "./src/components/ui/select";
