bbcc65902bab21db157dad0abcaf610ba4ed5b58
steven-yn <ssungyeon94@gmail.com>
2025-02-14 00:52:50 +0900

    chore: add toast

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -25,6 +25,7 @@ export * from "./src/components/ui/sonner";
 export * from "./src/components/ui/switch";
 export * from "./src/components/ui/tabs";
 export * from "./src/components/ui/textarea";
+export * from "./src/components/ui/toast";
 
 export * from "./src/components/typography/Heading";
 export * from "./src/components/typography/Large";
